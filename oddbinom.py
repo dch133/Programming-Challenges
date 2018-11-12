0fd5cb446bc7905edc1e133e1c138db590b4d00e
Daniel Chernis <32421770+dch133@users.noreply.github.com>
2018-11-11 23:05:54 -0500

    Add files via upload

--- oddbinom.py
+++ oddbinom.py
@@ -1,10 +1,11 @@
-
 n = int(input())
 total = 0
-mybin = bin
-#loop through all rows until n and add 2^(#1s in binary form) (theorem)
-for i in range (n):
-    numOdd = 2**(mybin(i).count("1"))
-    total+=numOdd
-    
+curr_mult_of_3 = 1
+row_in_bin = bin(n)[2:] #convert to binary
+for i in row_in_bin[::-1]: #loop through each bit (only consider 1s)
+    if (i is not "0"):
+        total *= 2
+        total += curr_mult_of_3
+    curr_mult_of_3 *= 3 #get the next multiple of 3
+
 print (total)
